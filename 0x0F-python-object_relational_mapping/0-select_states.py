6e0fdef5f9fe51f9e9e533b2e1d1f69f294caf03
EmRowland <65784893+EmRowland@users.noreply.github.com>
2024-06-22 23:38:34 -0800

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,11 +3,13 @@
 Lists all states from the database hbtn_0e_0_usa.
 """
 
+
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-    # Get MySQL credentials from command line arguments
+    # Get MySQL credentials from command line argument
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
