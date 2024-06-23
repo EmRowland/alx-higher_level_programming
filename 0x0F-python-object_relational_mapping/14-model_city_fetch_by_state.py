a1833c8d7bdda144e4172143a27b10bc674240f5
dantel <rowlanddantel@gmail.com>
2024-06-23 02:28:27 -0800

    made major edits

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,9 +9,6 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
@@ -27,18 +24,16 @@ if __name__ == "__main__":
 
     # Create session
     Session = sessionmaker(bind=engine)
-
+    Base.metadata.create_all(engine)
     # Context manager for session handling
     with Session() as session:
         # Query for all City objects, joining with State to get state names
-        cities = session.query(City).join(State).order_by(City.id).all()
+        cities = session.query(State, City) \
+            .filter(State.id == City.state_id)
 
-        if cities:
-            for city in cities:
-                print(
-                    "{}: ({}) {}".format(
-                        city.state.name,
-                        city.id,
-                        city.name))
-        else:
-            print("No cities found.")
+        for city in cities:
+            print(
+                "{}: ({}) {}".format(
+                    city.state.name,
+                    city.id,
+                    city.name))
